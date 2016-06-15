d7ea5f934ca39db5252d940f740f98a58706824b
Timothy Helton <timothy.j.helton@gmail.com>
2016-06-14 22:21:28 -0600

    Added method to translate note designation
    * dictionary keys are Lilypond values are Scientific Designation

--- note_generator.py
+++ note_generator.py
@@ -19,14 +19,16 @@ class Instrument:
     """Instrument Base Class
 
     :param str clef: clef for instrument music
-    :param str pitch_low: lowest possible pitch in Lilypond format
-    :param str pitch_high: highest possible pitch in Lilypond format
+    :param str pitch_low: lowest possible pitch in scientific designation
+    :param str pitch_high: highest possible pitch in scientific designation
     """
     def __init__(self, clef, pitch_low, pitch_high):
         self.scale = ('c', 'd', 'e', 'f', 'g', 'a', 'b')
-        self.octave = (",,", ",", "'", "''", "'''")
-        self.notes = [''.join(x) for x in
-                      list(itertools.product(self.scale, self.octave))]
+        self.octave = (",,,", ",,", ",", "", "'", "''", "'''", "''''",
+                       "'''''", "''''''")
+        self.notes_lilypond = [''.join(x) for x in
+                               list(itertools.product(self.scale, self.octave))]
+        self.notes = None
         self.clef = clef
         self.pitch_low = pitch_low
         self.pitch_high = pitch_high
@@ -37,21 +39,48 @@ class Instrument:
         self.pitch_range = self.notes[self.notes.index(self.pitch_low):
                                       self.notes.index(self.pitch_high)]
 
-    def note_images(self):
-        """Create note image using Lilypond."""
-        for note in self.pitch_range:
-            lilypond_input = '''\\version "2.18.2"
-            \\header {{
-                tagline = ""
-            }}
-            \\absolute {{
-                \\language "english"
-                \\override Staff.TimeSignature #'stencil = ##f
-                \\clef "{}"
-                {}
-            }}'''.format(self.clef, note)
+    def convert_octave(self):
+        """Convert Lilypond octave notation to scientific designation."""
+        note = self.notes_lilypond
+        note = [x.replace(",,,", '0') for x in note]
+        note = [x.replace(",,", '1') for x in note]
+        note = [x.replace(",", '2') for x in note]
+        note = [x.replace("''''''", '9') for x in note]
+        note = [x.replace("'''''", '8') for x in note]
+        note = [x.replace("''''", '7') for x in note]
+        note = [x.replace("'''", '6') for x in note]
+        note = [x.replace("''", '5') for x in note]
+        note = [x.replace("'", '4') for x in note]
+        note = [x + '3' if x.isalpha() else x for x in note]
 
-            # TODO call subprocess.Popen to run Lilypond and create note images
+        self.notes = {x: y for (x, y) in zip(self.notes_lilypond, note)}
+
+    def create_images(self):
+        """Create note images using Lilypond."""
+        os.makedirs(self.clef, exist_ok=True)
+        for pitch in self.pitch_range:
+            file_name = osp.join(self.clef, '{}.ly'.format(pitch))
+
+            with open(file_name, 'w') as f:
+                f.write(self.lilypond_input(pitch))
+
+    def lilypond_input(self, note):
+        """Generate Lilypond input file to create note image.
+
+        :param str note: musical note in Lilypond format
+        :returns: Lilypond commands to create a note image
+        :rtype: str
+        """
+        return '''\\version "2.18.2"
+                  \\header {{
+                      tagline = ""
+                  }}
+                  \\absolute {{
+                      \\language "english"
+                      \\override Staff.TimeSignature #'stencil = ##f
+                      \\clef "{}"
+                      {}
+                  }}'''.format(self.clef, note)
 
     def crop_images(self):
         """Crop the Lilypond images uniformly."""
@@ -59,7 +88,7 @@ class Instrument:
 
 
 if __name__ == '__main__':
-    violin = Instrument('treble', 'g4', "a'''4")
-    viola = Instrument('alto', 'c4', "g'''4")
-    cello = Instrument('bass', 'c4', "c'''4")
-    bass = Instrument('bass', 'e4', "c''4")
+    violin = Instrument('treble', 'g3', "a6")
+    viola = Instrument('alto', 'c3', "d6")
+    cello = Instrument('bass', 'c2', "c6")
+    bass = Instrument('bass', 'e1', "c5")
